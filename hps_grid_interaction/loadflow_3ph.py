97f76c7919733335619c5ded37fa53e3ddff6fb9
ss391347 <sebastian.schwarz1@rwth-aachen.de>
2024-01-16 14:41:55 +0100

    adapted loadflow Python script to new input format and changed output format to JSON

--- hps_grid_interaction/loadflow_3ph.py
+++ hps_grid_interaction/loadflow_3ph.py
@@ -1,7 +1,7 @@
 import json
 import os
-import math
-import pathlib
+import uuid
+import shutil
 import multiprocessing as mp
 import openpyxl
 import numpy as np
@@ -17,39 +17,31 @@ from pandapower.timeseries.data_sources.frame_data import DFData
 
 def run_lastfluss_simulation(
         trafo_kva: list,
-        all_results_path: pathlib.Path,
+        loadflow_scenarios: list,
         cos_phi: float = 0.95,
         n_cpu: int = 1,
         sheet_name: str = "lastfluss"
 ):
-    with open(all_results_path, "r") as file:
-        all_results = json.load(file)
-    files = []
-    save_paths = []
-    for quota_case, input_path in all_results["grid_simulation_inputs"].items():
-        files.append(pathlib.Path(input_path))
-        save_paths.append(pathlib.Path(input_path).parent)
+    count = 0
     simulations_to_run = []
     for kva in trafo_kva:
-        for file, save_path in zip(files, save_paths):
-            case = file.stem
-            if cos_phi != 0.95:
-                save_path_case = save_path.joinpath(f"3-ph-{(int(cos_phi*100))}", f"{case}_{int(kva)}")
-            else:
-                save_path_case = save_path.joinpath("3-ph", f"{case}_{int(kva)}")
-            if save_path_case.exists():
-                print(f"Won't run sheet {save_path_case.name}, folder already exists.")
-                continue
-            if save_path_case.name.startswith("Mon_"):  # TODO: Revert
-                print("Skipping Simulation of monovalent cases for GEG-update:", save_path_case)
+        for file in loadflow_scenarios:
+            count += 1
+            path = os.path.dirname(file)
+            filename = os.path.basename(file)
+            case_name = os.path.abspath(
+                os.path.join(path,
+                             "results_" + str(str(os.path.splitext(filename)[0]) + "_3-ph_" + str(int(kva)) + ".json"))
+            )
+            if os.path.exists(case_name):
+                print("Won't run simulation " + str(filename) + ", solution JSON file already exists.")
                 continue
             workbook = openpyxl.load_workbook(file)
             ws = workbook[sheet_name]
             simulations_to_run.append(dict(
-                worksheet=ws, kva=kva, case=case, cos_phi=cos_phi,
-                save_path=save_path_case,
-                with_plot=False
-            ))
+                worksheet=ws, kva=kva, case=str(filename), cos_phi=cos_phi,
+                save_path=case_name, with_plot=False, case_no=count)
+            )
     if n_cpu == 1:
         for simulation_to_run in simulations_to_run:
             run_single_worksheet(simulation_to_run)
@@ -72,60 +64,34 @@ def run_single_worksheet(
     print("Conducting the load flow simulation for scenario " + str(case) + " with trafo size " +
           str(int(kva)) + "kVA.")
 
+    random_str = str(uuid.uuid4())
+
     connection_points = np.empty(len(worksheet["A"]) - 1, dtype=object)
     elec_demand_files = np.empty(len(worksheet["A"]) - 1, dtype=object)
-    hp_demand_files = np.empty(len(worksheet["A"]) - 1, dtype=object)
-    ev_demand_files = np.empty(len(worksheet["A"]) - 1, dtype=object)
 
     for i in range(len(worksheet["A"]) - 1):
         connection_points[i] = "loadbus_" + str(worksheet["D"][i + 1].value).replace("-", "_") + ""
-        elec_demand_files[i] = str(worksheet["H"][i + 1].value)
-        hp_demand_files[i] = str(worksheet["I"][i + 1].value)
-        ev_demand_files[i] = str(worksheet["J"][i + 1].value)
+        elec_demand_files[i] = str(worksheet["I"][i + 1].value)
 
     elec_p_demand_timeseries = np.empty_like(elec_demand_files)
-    hp_p_demand_timeseries = np.empty_like(hp_demand_files)
-    ev_p_demand_timeseries = np.empty_like(ev_demand_files)
-
     elec_q_demand_timeseries = np.empty_like(elec_demand_files)
-    hp_q_demand_timeseries = np.empty_like(hp_demand_files)
-    ev_q_demand_timeseries = np.empty_like(ev_demand_files)
 
     # load time series CSV files with loads (this will take a while!)
     for i in range(len(worksheet["A"]) - 1):
-        print("Preparing input " + str(i + 1) + "/" + str(len(worksheet["A"])) + ".")
+        print("Preparing input " + str(i + 1) + "/" + str(len(worksheet["A"])-1) + ".")
 
-        elec_p_demand_timeseries[i] = pd.read_csv(os.path.abspath(elec_demand_files[i]), usecols=[1],
-                                                  names=["power"], header=None)
-        hp_p_demand_timeseries[i] = pd.read_csv(os.path.abspath(hp_demand_files[i]), usecols=[1],
-                                                names=["power"], header=None)
-        ev_p_demand_timeseries[i] = pd.read_csv(os.path.abspath(ev_demand_files[i]), usecols=[1],
-                                                names=["power"], header=None)
+        elec_p_demand_timeseries[i] = pd.read_csv(
+            os.path.abspath(os.path.join(str(os.path.dirname(save_path)), str(elec_demand_files[i])).
+                            replace("\\","/")), usecols=[1], names=["power"], header=None
+        )
 
         elec_p_demand_timeseries[i].drop(elec_p_demand_timeseries[i].head(1).index, inplace=True)
-        hp_p_demand_timeseries[i].drop(hp_p_demand_timeseries[i].head(1).index, inplace=True)
-        hp_p_demand_timeseries[i].drop(hp_p_demand_timeseries[i].tail(1).index, inplace=True)
-        ev_p_demand_timeseries[i].drop(ev_p_demand_timeseries[i].head(1).index, inplace=True)
-
         elec_p_demand_timeseries[i].power = elec_p_demand_timeseries[i].power.astype(float)
-        hp_p_demand_timeseries[i].power = hp_p_demand_timeseries[i].power.astype(float)
-        ev_p_demand_timeseries[i].power = ev_p_demand_timeseries[i].power.astype(float)
 
         elec_q_demand_timeseries[i] = elec_p_demand_timeseries[i].copy()
-        hp_q_demand_timeseries[i] = hp_p_demand_timeseries[i].copy()
-        ev_q_demand_timeseries[i] = ev_p_demand_timeseries[i].copy()
-
         elec_q_demand_timeseries[i]["power"] = \
             np.sqrt((elec_q_demand_timeseries[i]["power"] / cos_phi) ** 2 -
                     elec_q_demand_timeseries[i]["power"] ** 2)
-
-        hp_q_demand_timeseries[i]["power"] = \
-            np.sqrt((hp_q_demand_timeseries[i]["power"] / cos_phi) ** 2 -
-                    hp_q_demand_timeseries[i]["power"] ** 2)
-
-        ev_q_demand_timeseries[i]["power"] = \
-            np.sqrt((ev_q_demand_timeseries[i]["power"] / cos_phi) ** 2 -
-                    ev_q_demand_timeseries[i]["power"] ** 2)
 
     # load a pandapower network
     net = nw.create_kerber_vorstadtnetz_kabel_1()
@@ -144,14 +110,10 @@ def run_single_worksheet(
     df_reactive = pd.DataFrame()
     for i in range(len(worksheet["A"]) - 1):
         load_id = loadbusdict[connection_points[i]]
-        df_active[load_id] = elec_p_demand_timeseries[load_id]["power"] + \
-                             hp_p_demand_timeseries[load_id]["power"] + \
-                             ev_p_demand_timeseries[load_id]["power"]
+        df_active[load_id] = elec_p_demand_timeseries[load_id]["power"]
         df_active[load_id] = df_active[load_id].multiply(1.0 / 3.0)
         df_active[load_id] = df_active[load_id].multiply(1.0 / 1000.0)
-        df_reactive[load_id] = elec_q_demand_timeseries[load_id]["power"] + \
-                               hp_q_demand_timeseries[load_id]["power"] + \
-                               ev_q_demand_timeseries[load_id]["power"]
+        df_reactive[load_id] = elec_q_demand_timeseries[load_id]["power"]
         df_reactive[load_id] = df_reactive[load_id].multiply(1.0 / 3.0)
         df_reactive[load_id] = df_reactive[load_id].multiply(1.0 / 1000.0)
     df_active = df_active.reindex(sorted(df_active.columns), axis=1)
@@ -168,11 +130,11 @@ def run_single_worksheet(
     reactive_load = control.ConstControl(net, element='load', element_index=net.load.index,
                                          variable='q_mvar', data_source=ds_reactive, profile_name=net.load.index)
 
-    # initialising the outputwriter to save data to excel files in the current folder
+    # initialising the outputwriter to save data to JSON files in the specified folder(s)
     # You can change this to .json, .csv, or .pickle as well
     ow = timeseries.OutputWriter(net,
-                                 output_path=str(save_path),
-                                 output_file_type=".xlsx")
+                                 output_path=os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str)),
+                                 output_file_type=".json")
     # adding vm_pu of all buses and line_loading in percent of all lines as outputs to be stored
     ow.log_variable('res_bus', 'p_mw')
     ow.log_variable('res_bus', 'q_mvar')
@@ -181,27 +143,73 @@ def run_single_worksheet(
 
     # starting the timeseries simulation for one year in 15 min values.
     print("Performing the load flow simulation.")
-    timeseries.run_timeseries(net)
+    timeseries.run_timeseries(net, numba=False)
 
     print("Load flow simulation successful.")
     if with_plot:
         # Finally and if desired, create an HTML plot
         plotting.to_html(net, "kerber.html")
 
+    print("Writing results.")
+    p_trafo = np.array([0.0 for i in range(len(df_active))])
+    q_trafo = np.array([0.0 for i in range(len(df_active))])
+    vm_pu_min = np.array([np.infty for i in range(len(df_active))])
+    max_line_loading = np.array([0.0 for i in range(len(df_active))])
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_bus",
+                                           "p_mw.json")), 'r') as j:
+        p_mw = json.loads(j.read())
+    count = 0
+    for key, value in p_mw["0"].items():
+        p_trafo[count] = (-1.0) * value * 1000.0
+        count += 1
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_bus",
+                                           "q_mvar.json")), 'r') as j:
+        q_mvar = json.loads(j.read())
+    count = 0
+    for key, value in q_mvar["0"].items():
+        q_trafo[count] = (-1.0) * value * 1000.0
+        count += 1
+
+    s_trafo = np.array([np.sqrt(p_trafo[i]**2 + q_trafo[i]**2) for i in range(len(df_active))])
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_bus",
+                                           "vm_pu.json")), 'r') as j:
+        vm_pu = json.loads(j.read())
+    for key_outer, value_outer in vm_pu.items():
+        count = 0
+        for key_inner, value_inner in vm_pu[key_outer].items():
+            vm_pu_min[count] = min(vm_pu_min[count], value_inner)
+            count += 1
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_line",
+                                           "loading_percent.json")), 'r') as j:
+        loading_percent = json.loads(j.read())
+    for key_outer, value_outer in loading_percent.items():
+        count = 0
+        for key_inner, value_inner in loading_percent[key_outer].items():
+            max_line_loading[count] = max(max_line_loading[count], value_inner)
+            count += 1
+
+    results_dict = dict(p_trafo=list(p_trafo), q_trafo=list(q_trafo), s_trafo=list(s_trafo),
+                        vm_pu_min=list(vm_pu_min), max_line_loading=list(max_line_loading))
+    with open(os.path.abspath(
+            os.path.join(str(os.path.dirname(save_path)),
+                         "results_" + str(case).split(".")[0] + "_3-ph_" + str(int(kva)) + ".json")), 'w') as k:
+        json.dump(results_dict, k)
+
+    # Finally remove all temporary files:
+    shutil.rmtree(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str)))
+    print("Success.")
+
 
 if __name__ == '__main__':
-    from hps_grid_interaction import RESULTS_MONTE_CARLO_FOLDER
-    for quota_study in [
-        "av_e_mob_with_pv_bat",
-        "av_heat_pump",
-        "av_pv_bat",
-        "av_hyb",
-        "av_pv",
-        "av_hyb_with_pv_bat",
-    ]:
-        run_lastfluss_simulation(
-            trafo_kva=[400, 630.0, 1000, 2000],
-            all_results_path=RESULTS_MONTE_CARLO_FOLDER.joinpath(f"Altbau_{quota_study}", "results_to_plot.json"),
-            n_cpu=2,
-            cos_phi=0.95
-        )
+    from loadflow_scenarios import LOADFLOW_SCENARIOS
+
+    run_lastfluss_simulation(
+        trafo_kva=[1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0],
+        loadflow_scenarios=LOADFLOW_SCENARIOS,
+        n_cpu=1,
+        cos_phi=0.95
+    )
---
+++ hps_grid_interaction/loadflow_3ph_mpi.py
@@ -0,0 +1,223 @@
+import json
+import os
+import uuid
+import shutil
+import openpyxl
+import numpy as np
+import pandas as pd
+from mpi4py import MPI
+
+# We are using pandapower 2.13.1
+import pandapower.control as control
+import pandapower.networks as nw
+import pandapower.timeseries as timeseries
+import pandapower.plotting as plotting
+from pandapower.timeseries.data_sources.frame_data import DFData
+
+
+def run_lastfluss_simulation(
+        trafo_kva: list,
+        loadflow_scenarios: list,
+        cos_phi: float = 0.95,
+        sheet_name: str = "lastfluss"
+):
+    try:
+        comm = MPI.COMM_WORLD
+        rank = comm.Get_rank()
+        size = comm.Get_size()
+    except:
+        comm = None
+        rank = 0
+        size = 1
+
+    count = 0
+    simulations_to_run = []
+    for kva in trafo_kva:
+        for file in loadflow_scenarios:
+            count += 1
+            path = os.path.dirname(file)
+            filename = os.path.basename(file)
+            case_name = os.path.abspath(
+                os.path.join(path,
+                             "results_" + str(str(os.path.splitext(filename)[0]) + "_3-ph_" + str(int(kva)) + ".json"))
+            )
+            if os.path.exists(case_name):
+                print("Won't run simulation " + str(filename) + ", solution JSON file already exists.")
+                continue
+            workbook = openpyxl.load_workbook(file)
+            ws = workbook[sheet_name]
+            simulations_to_run.append(dict(
+                worksheet=ws, kva=kva, case=str(filename), cos_phi=cos_phi,
+                save_path=case_name, with_plot=False, case_no=count)
+            )
+    if size == 1:
+        for simulation_to_run in simulations_to_run:
+            run_single_worksheet(simulation_to_run)
+    else:
+        for simulation_to_run in simulations_to_run:
+            if rank == divmod(simulation_to_run["case_no"], size)[1]:
+                print("Rank " + str(rank) + ": Running case " + str(simulation_to_run["case"]) + " with trafo size "
+                      + str(int(simulation_to_run["kva"])) + "kVA.")
+                run_single_worksheet(simulation_to_run)
+
+
+def run_single_worksheet(
+    simulation_to_run: dict
+):
+    worksheet = simulation_to_run["worksheet"]
+    kva = simulation_to_run["kva"]
+    case = simulation_to_run["case"]
+    cos_phi = simulation_to_run["cos_phi"]
+    save_path = simulation_to_run["save_path"]
+    with_plot = simulation_to_run["with_plot"]
+    print("Conducting the load flow simulation for scenario " + str(case) + " with trafo size " +
+          str(int(kva)) + "kVA.")
+
+    random_str = str(uuid.uuid4())
+
+    connection_points = np.empty(len(worksheet["A"]) - 1, dtype=object)
+    elec_demand_files = np.empty(len(worksheet["A"]) - 1, dtype=object)
+
+    for i in range(len(worksheet["A"]) - 1):
+        connection_points[i] = "loadbus_" + str(worksheet["D"][i + 1].value).replace("-", "_") + ""
+        elec_demand_files[i] = str(worksheet["I"][i + 1].value)
+
+    elec_p_demand_timeseries = np.empty_like(elec_demand_files)
+    elec_q_demand_timeseries = np.empty_like(elec_demand_files)
+
+    # load time series CSV files with loads (this will take a while!)
+    for i in range(len(worksheet["A"]) - 1):
+        print("Preparing input " + str(i + 1) + "/" + str(len(worksheet["A"])-1) + ".")
+
+        elec_p_demand_timeseries[i] = pd.read_csv(
+            os.path.abspath(os.path.join(str(os.path.dirname(save_path)), str(elec_demand_files[i])).
+                            replace("\\","/")), usecols=[1], names=["power"], header=None
+        )
+
+        elec_p_demand_timeseries[i].drop(elec_p_demand_timeseries[i].head(1).index, inplace=True)
+        elec_p_demand_timeseries[i].power = elec_p_demand_timeseries[i].power.astype(float)
+
+        elec_q_demand_timeseries[i] = elec_p_demand_timeseries[i].copy()
+        elec_q_demand_timeseries[i]["power"] = \
+            np.sqrt((elec_q_demand_timeseries[i]["power"] / cos_phi) ** 2 -
+                    elec_q_demand_timeseries[i]["power"] ** 2)
+
+    # load a pandapower network
+    net = nw.create_kerber_vorstadtnetz_kabel_1()
+    net.trafo.sn_mva = kva / 1000.0 / 3.0
+    net.trafo.tap_min = 0
+    net.trafo.tap_max = 0
+
+    # assemble the loads from the different CSV files and make it fit to the pandapower's Kerber grid model
+    busdict = net.bus.to_dict()
+    loadbusdict = dict()
+    for key, value in busdict["name"].items():
+        if str(value).startswith("loadbus"):
+            loadbusdict[str(value)] = int(net.load.bus[net.load.bus == key].index[0])
+
+    df_active = pd.DataFrame()
+    df_reactive = pd.DataFrame()
+    for i in range(len(worksheet["A"]) - 1):
+        load_id = loadbusdict[connection_points[i]]
+        df_active[load_id] = elec_p_demand_timeseries[load_id]["power"]
+        df_active[load_id] = df_active[load_id].multiply(1.0 / 3.0)
+        df_active[load_id] = df_active[load_id].multiply(1.0 / 1000.0)
+        df_reactive[load_id] = elec_q_demand_timeseries[load_id]["power"]
+        df_reactive[load_id] = df_reactive[load_id].multiply(1.0 / 3.0)
+        df_reactive[load_id] = df_reactive[load_id].multiply(1.0 / 1000.0)
+    df_active = df_active.reindex(sorted(df_active.columns), axis=1)
+    df_active.index = range(0, len(df_active))
+    df_reactive = df_reactive.reindex(sorted(df_reactive.columns), axis=1)
+    df_reactive.index = range(0, len(df_reactive))
+
+    # update the loads of the pandapower Kerber grid
+    ds_active = DFData(df_active)
+    active_load = control.ConstControl(net, element='load', element_index=net.load.index,
+                                       variable='p_mw', data_source=ds_active, profile_name=net.load.index)
+
+    ds_reactive = DFData(df_reactive)
+    reactive_load = control.ConstControl(net, element='load', element_index=net.load.index,
+                                         variable='q_mvar', data_source=ds_reactive, profile_name=net.load.index)
+
+    # initialising the outputwriter to save data to JSON files in the specified folder(s)
+    # You can change this to .json, .csv, or .pickle as well
+    ow = timeseries.OutputWriter(net,
+                                 output_path=os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str)),
+                                 output_file_type=".json")
+    # adding vm_pu of all buses and line_loading in percent of all lines as outputs to be stored
+    ow.log_variable('res_bus', 'p_mw')
+    ow.log_variable('res_bus', 'q_mvar')
+    ow.log_variable('res_bus', 'vm_pu')
+    ow.log_variable('res_line', 'loading_percent')
+
+    # starting the timeseries simulation for one year in 15 min values.
+    print("Performing the load flow simulation.")
+    timeseries.run_timeseries(net, numba=False)
+
+    print("Load flow simulation successful.")
+    if with_plot:
+        # Finally and if desired, create an HTML plot
+        plotting.to_html(net, "kerber.html")
+
+    print("Writing results.")
+    p_trafo = np.array([0.0 for i in range(len(df_active))])
+    q_trafo = np.array([0.0 for i in range(len(df_active))])
+    vm_pu_min = np.array([np.infty for i in range(len(df_active))])
+    max_line_loading = np.array([0.0 for i in range(len(df_active))])
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_bus",
+                                           "p_mw.json")), 'r') as j:
+        p_mw = json.loads(j.read())
+    count = 0
+    for key, value in p_mw["0"].items():
+        p_trafo[count] = (-1.0) * value * 1000.0
+        count += 1
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_bus",
+                                           "q_mvar.json")), 'r') as j:
+        q_mvar = json.loads(j.read())
+    count = 0
+    for key, value in q_mvar["0"].items():
+        q_trafo[count] = (-1.0) * value * 1000.0
+        count += 1
+
+    s_trafo = np.array([np.sqrt(p_trafo[i]**2 + q_trafo[i]**2) for i in range(len(df_active))])
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_bus",
+                                           "vm_pu.json")), 'r') as j:
+        vm_pu = json.loads(j.read())
+    for key_outer, value_outer in vm_pu.items():
+        count = 0
+        for key_inner, value_inner in vm_pu[key_outer].items():
+            vm_pu_min[count] = min(vm_pu_min[count], value_inner)
+            count += 1
+
+    with open(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str, "res_line",
+                                           "loading_percent.json")), 'r') as j:
+        loading_percent = json.loads(j.read())
+    for key_outer, value_outer in loading_percent.items():
+        count = 0
+        for key_inner, value_inner in loading_percent[key_outer].items():
+            max_line_loading[count] = max(max_line_loading[count], value_inner)
+            count += 1
+
+    results_dict = dict(p_trafo=list(p_trafo), q_trafo=list(q_trafo), s_trafo=list(s_trafo),
+                        vm_pu_min=list(vm_pu_min), max_line_loading=list(max_line_loading))
+    with open(os.path.abspath(
+            os.path.join(str(os.path.dirname(save_path)),
+                         "results_" + str(case).split(".")[0] + "_3-ph_" + str(int(kva)) + ".json")), 'w') as k:
+        json.dump(results_dict, k)
+
+    # Finally remove all temporary files:
+    shutil.rmtree(os.path.abspath(os.path.join(str(os.path.dirname(save_path)), random_str)))
+    print("Success.")
+
+
+if __name__ == '__main__':
+    from loadflow_scenarios import LOADFLOW_SCENARIOS
+
+    run_lastfluss_simulation(
+        trafo_kva=[1000.0, 1200.0, 1400.0, 1600.0, 1800.0, 2000.0],
+        loadflow_scenarios=LOADFLOW_SCENARIOS,
+        cos_phi=0.95
+    )

df3daa3f9a937949f5a5f54b1a92eeaf13e8dbda
fabian.wuellhorst <fabian.wuellhorst@rwth-aachen.de>
2024-08-05 17:55:35 +0200

    Update voltage drop plot

--- hps_grid_interaction/plotting/plot_voltage_drop.py
+++ hps_grid_interaction/plotting/plot_voltage_drop.py
@@ -9,7 +9,7 @@ import pandas as pd
 from hps_grid_interaction import KERBER_NETZ_XLSX
 
 
-def plot_voltage_drop():
+def plot_voltage_drop(with_all_feeders: bool = False):
     df_grid = pd.read_excel(KERBER_NETZ_XLSX, sheet_name="Kerber Netz oldbuildings", index_col=0)
 
     RESULTS_MONTE_CARLO_FOLDER = pathlib.Path(r"X:\Projekte\EBC_ACS0025_EONgGmbH_HybridWP_\Data\04_Ergebnisse\03_monte_carlo")
@@ -33,21 +33,30 @@ def plot_voltage_drop():
     index = np.arange(len(data["vm_pu_min"])) / 4 / 24
     voltage = data["vm_pu_min"]
     power = data["p_trafo"]
-    fig, ax = plt.subplots(3, 1, sharex=True)
-    ax[0].plot(index, voltage)
-    ax[0].set_ylabel("$v_\mathrm{min}$ in p.u.")
+    fig, ax = plt.subplots(4 if with_all_feeders else 3, 1, sharex=True)
 
-    ax[1].plot(index, power)
-    ax[1].plot(index, np.sum(list(feeder_sums.values()), axis=0), linestyle="--")
-    ax[1].set_ylabel("$P_\mathrm{el,Ges}$ in kW")
+    from hps_grid_interaction.utils import load_outdoor_air_temperature
+    t_oda_csv = load_outdoor_air_temperature()
+    t_oda_csv.index /= 24
+    ax[0].plot(t_oda_csv.index, t_oda_csv)
+    ax[0].set_ylabel("$T_\mathrm{Oda}$ in °C")
 
-    for idx, feeder in enumerate(feeder_sums):
-        ax[2].plot(index, feeder_sums[feeder], label=feeder)
-    ax[2].set_ylabel("$P_\mathrm{el}$ in kW")
-    ax[2].legend(ncol=2)
+    ax[1].plot(index, voltage)
+    ax[1].set_ylabel("$v_\mathrm{min}$ in p.u.")
+
+    ax[2].plot(index, power)
+    ax[2].plot(index, np.sum(list(feeder_sums.values()), axis=0), linestyle="--")
+    ax[2].set_ylabel("$P_\mathrm{el,Ges}$ in kW")
+    if with_all_feeders:
+        for idx, feeder in enumerate(feeder_sums):
+            ax[3].plot(index, feeder_sums[feeder], label=feeder)
+        #ax[3].plot(index, feeder_sums[9], label="9", marker="^")
+        ax[3].set_ylabel("$P_\mathrm{el}$ in kW")
+        ax[3].legend(ncol=2)
+
     ax[-1].set_xlabel("Time in d")
     plt.show()
 
 
 if __name__ == '__main__':
-    plot_voltage_drop()
+    plot_voltage_drop(with_all_feeders=True)
